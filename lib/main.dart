0428f1ac5ea84836afd8c2c0a65df603e1066568
Abdurrahim Kurt <krtabdurrahim@gmail.com>
2021-01-19 10:10:46 +0300

    son değişiklikler

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -18,6 +21,7 @@ class LandingScreen extends StatefulWidget {
 
 class _LandingScreenState extends State<LandingScreen> {
   File imageFile;
+  String sifreliFile;
 
   _openGallary(BuildContext context) async {
     var picture =
@@ -69,9 +73,13 @@ class _LandingScreenState extends State<LandingScreen> {
 
   Widget _decideImageView() {
     if (imageFile == null) {
-      return Text("Henüz resim Seçilmedi!");
+      return Text(
+        "Henüz resim Seçilmedi!",
+        style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+      );
     } else {
-      return Image.file(imageFile, width: 400, height: 400);
+      return Container(
+          height: 200, child: Image.file(imageFile, width: 400, height: 400));
     }
   }
 
@@ -81,6 +89,7 @@ class _LandingScreenState extends State<LandingScreen> {
       appBar: AppBar(
         title: Text("YMGK2"),
         centerTitle: mounted,
+        backgroundColor: Colors.orange[700],
       ),
       body: Container(
         child: Center(
@@ -89,15 +98,70 @@ class _LandingScreenState extends State<LandingScreen> {
             children: <Widget>[
               _decideImageView(),
               RaisedButton(
+                color: Colors.orange[700],
                 onPressed: () {
                   _showChoiceDiolog(context);
                 },
-                child: Text("Resim Ekle"),
+                child: Text(
+                  "Resim Ekle",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19.0),
+                ),
+              ),
+              _sifreliImage(),
+              RaisedButton(
+                color: Colors.orange[700],
+                onPressed: () {
+                  _sifrele();
+                },
+                child: Text(
+                  "Şifrele",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19.0),
+                ),
               )
             ],
           ),
         ),
       ),
     );
+  }
+
+  var yeniimage;
+
+  _sifrele() async {
+    print(imageFile.path);
+    var bytes = (await rootBundle.load(imageFile.path)).buffer.asUint8List();
+    
+    var mpFile =
+        http.MultipartFile.fromBytes('photo', bytes, filename: 'tolga.jpg');
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('http://webmenudemo.online/api/File/Upload'));
+    request.files.add(mpFile);
+
+    request.send().then((response) {
+      if (response.statusCode == 200) {
+        response.stream.bytesToString().then((value) {
+          setState(() {
+            yeniimage = "erqweqwewq";
+          });
+        });
+      }
+    });
+  }
+
+  Widget _sifreliImage() {
+    if (yeniimage != null) {
+      return Container(
+        height: 200,
+        child: Image.network(yeniimage),
+      );
+    } else {
+      return Text("Bekleniyor..");
+    }
   }
 }
